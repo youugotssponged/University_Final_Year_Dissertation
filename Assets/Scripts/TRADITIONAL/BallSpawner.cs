24f484966c7d5819e7d6f3cfd568d850c278d239
InfiniteShockz <jordanmccann64@outlook.com>
2020-03-11 16:31:55 +0000

    Traditional Prototype progress

---
+++ Assets/Scripts/TRADITIONAL/BallSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+
+    private void FixedUpdate()
+    {
+        objectPooler.SpawnFromPool("Ball", transform.position, Quaternion.identity);
+    }
+}
